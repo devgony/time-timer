1d7d2776940a6540fc903d2ea88a06b65ec7bb41
devgony <devgony@gmail.com>
2022-01-25 16:11:52 +0900

    Ticker with use_futre, use_ref

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use dioxus::prelude::*;
+use gloo_timers::future::TimeoutFuture;
 
 fn main() {
     dioxus::web::launch(app);
 }
 
 fn app(cx: Scope) -> Element {
-    let mut count = use_state(&cx, || 5);
+    let sec = use_ref(&cx, || 5);
+    use_future(&cx, || {
+        let sec = sec.clone(); // future static var should be cloned
+        async move {
+            loop {
+                TimeoutFuture::new(1_000).await;
+                *sec.write() += 5;
+            }
+        }
+    });
+    let cur_sec = sec.read();
     cx.render(rsx!(
         body {
             margin: "0",
@@ -22,12 +33,13 @@ fn app(cx: Scope) -> Element {
                 height: "50vh",
                 background_color: "white",
                 border_radius: "50%",
-                div {class: "pie no-round", style: "--p:{count};--b:100%;--c:red;--w:50vh;"}
+                div {class: "pie no-round", style: "--p:{cur_sec};--b:100%;--c:red;--w:50vh;"}
             }
             div {
-                button { onclick: move |_| count += 5, "+" }
-                button { onclick: move |_| count -= 5, "-" }
-                button { onclick: move |_| count -= 0, "Start" }
+                h1 {"{cur_sec}"}
+                // button { onclick: move |_| {sec += 5}, "+" }
+                // button { onclick: move |_| set_state(sec - 5), "-" }
+                // button { onclick: move |_| set_state(sec + 0), "Start" }
             }
         }
     ))
