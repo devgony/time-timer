c7d2aabb9c81eaf6f240c0372487d582cdd2b88d
devgony <devgony@gmail.com>
2022-01-25 10:23:22 +0900

    pie chart css with variable

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,7 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let mut count = use_state(&cx, || 125);
-    let num = if count.le(&270) {
-        count + 0
-    } else {
-        count - 180
-    };
-    let color = if count.le(&270) { "white" } else { "red" };
+    let mut count = use_state(&cx, || 30);
     cx.render(rsx!(
         body {
             margin: "0",
@@ -22,30 +16,11 @@ fn app(cx: Scope) -> Element {
             flex_direction: "column",
             align_items: "center",
             justify_content: "center",
+            style { [include_str!("../src/style.css")] }
+            div {class: "pie no-round", style: "--p:{count};--b:100%;--c:red;--w:50%;"}
             div {
-                background_color: "red",
-                border: "1px solid blue",
-                width: "50vh",
-                height: "50vh",
-                border_radius: "50%",
-                display: "flex",
-                align_items: "center",
-                justify_content: "center",
-
-                background_image:
-                    "linear-gradient({num}deg, transparent 50%, {color} 50%),
-                    linear-gradient(90deg, white 50%, transparent 50%)",
-                // margin: "auto",
-                // div {
-                //     background_color: "red",
-                //     width: "49vh",
-                //     height: "49vh",
-                //     border_radius: "50%",
-                // }
-            }
-            div {
-                button { onclick: move |_| count += 10, "Start" }
-                button { onclick: move |_| count -= 10, "Stop" }
+                button { onclick: move |_| count += 1, "Start" }
+                button { onclick: move |_| count -= 1, "Stop" }
             }
         }
     ))
