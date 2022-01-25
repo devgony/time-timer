65b133b62463a9351cd13c697103bf12ffe085f9
devgony <devgony@gmail.com>
2022-01-25 10:35:34 +0900

    add white panel, scaleX

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,29 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let mut count = use_state(&cx, || 30);
+    let mut count = use_state(&cx, || 5);
     cx.render(rsx!(
         body {
             margin: "0",
-            width: "100%",
-            height: "100%",
+            width: "100vw",
+            height: "100vh",
             background_color: "gray",
             display: "flex",
             flex_direction: "column",
             align_items: "center",
             justify_content: "center",
             style { [include_str!("../src/style.css")] }
-            div {class: "pie no-round", style: "--p:{count};--b:100%;--c:red;--w:50%;"}
             div {
-                button { onclick: move |_| count += 1, "Start" }
-                button { onclick: move |_| count -= 1, "Stop" }
+                width: "50vh",
+                height: "50vh",
+                background_color: "white",
+                border_radius: "50%",
+                div {class: "pie no-round", style: "--p:{count};--b:100%;--c:red;--w:50vh;"}
+            }
+            div {
+                button { onclick: move |_| count += 5, "+" }
+                button { onclick: move |_| count -= 5, "-" }
+                button { onclick: move |_| count -= 0, "Start" }
             }
         }
     ))
